f5675713f895e4d3372b417330612a4e183f4704
Narayan Thakare <narayan111thakare@gmail.com>
2023-05-15 10:42:58 +0530

    First commit

---
+++ src/heartdesine/Heart.java
@@ -0,0 +1,25 @@
+package heartdesine;
+
+public class Heart {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		  System.out.println("      * * *           *  * *");
+	      System.out.println("    *       *       *        *");
+	      System.out.println("  *           *   *            *");
+	      System.out.println(" *              *               *");
+	      System.out.println("*                                *");
+	      System.out.println("*      WELCOME TO MY PROJECT     *");
+	      System.out.println(" *                               *");
+	      System.out.println("  *                             *");
+	      System.out.println("    *                         *");
+	      System.out.println("      *                     *");
+	      System.out.println("        *                 *");
+	      System.out.println("           *           *");
+	      System.out.println("              *     *");
+	      System.out.println("                 *");
+
+	}
+
+}
