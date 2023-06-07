23c9873087e5443ce6f065d38355b08fa3d4daf5
Harshal Meher <134125835+Ronney29@users.noreply.github.com>
2023-06-07 10:58:49 +0530

    Update Heart.java

--- src/heartdesine/Heart.java
+++ src/heartdesine/Heart.java
@@ -5,7 +5,7 @@ public class Heart {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		  System.out.println("      * * *           *  * *");
+	      System.out.println("      * * *           * * *");
 	      System.out.println("    *       *       *        *");
 	      System.out.println("  *           *   *            *");
 	      System.out.println(" *              *               *");
